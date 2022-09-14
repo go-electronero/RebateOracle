56fc1773c15950c43848d9d0093feb54c01a4fe0
Bill <38635290+billaure-crystaleum@users.noreply.github.com>
2022-09-14 15:51:27 -0400

    Update STACK_DAO.sol

--- development/STACK_DAO.sol
+++ development/STACK_DAO.sol
@@ -11,8 +11,22 @@ pragma solidity 0.8.13;
  */
 
 import "./Auth.sol";
-import "./RebateOracle.sol";
 
+interface IERC20 {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+}
 /**
  * Interchained STACKOne aka "STACK"
  * DAO-STACK Contract
